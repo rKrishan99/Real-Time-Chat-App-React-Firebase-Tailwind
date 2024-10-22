eef69f52c236bf8c377f8d53c415bc8d6b4a3194
RajithaKWG <rkrishan894@gmail.com>
2024-10-22 20:44:50 +0530

    styled basic items

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -1,12 +1,13 @@
-
+import { Fragment } from "react";
 
 function App() {
-
   return (
-    <div>
-      
+    <div class="h-screen w-screen flex items-center justify-center">
+      <div className="w-11/12 h-5/6 bg-slate-800 bg-opacity-80 rounded-lg backdrop-blur-sm backdrop-saturate-50 border-2 border-neutral-800 border-opacity-40">
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- chat-app/tailwind.config.js
+++ chat-app/tailwind.config.js
@@ -9,8 +9,12 @@ export default {
       fontFamily: {
         lucida: [
           "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"
-        ]
-      }
+        ],
+      },
+      width:{
+        "90vw": "90vw",
+        "90vh": "90vh",
+      },
     },
   },
   plugins: [],
