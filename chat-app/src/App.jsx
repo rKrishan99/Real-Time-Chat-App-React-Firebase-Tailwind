58e49cf8c684f8f5cbe98b103dc7231ef0db1a33
RajithaKWG <rkrishan894@gmail.com>
2024-10-23 14:04:19 +0530

    Installed React-Toastify

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -2,18 +2,27 @@ import { Fragment } from "react";
 import Details from "./components/details/Details";
 import Chat from "./components/chat/Chat";
 import List from "./components/list/List";
+import Login from "./components/login/Login";
 
 function App() {
+  const user = false;
+
   return (
-    <div class="h-screen w-screen flex items-center justify-center">
-      <div className="w-full h-full bg-slate-800 bg-opacity-80 backdrop-blur-sm backdrop-saturate-50 border-2 border-neutral-800 border-opacity-40
-      flex ">
-        <List/>
-        <Chat/>
-        <Details/>
-      </div>
+    <div
+      className="w-screen h-screen bg-slate-800 bg-opacity-80 backdrop-blur-sm backdrop-saturate-50 border-2 border-neutral-800 border-opacity-40
+      flex "
+    >
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Details />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
-}
+};
 
 export default App;
